e31a83fcc8479b5e1cd419973f3c3de9d9c91976
omkarsachdeva <125148365+omkarsachdeva@users.noreply.github.com>
2024-06-06 19:10:27 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:unsad_app/view/mainScreen.dart';
+import 'package:unsad_app/view/splashScreen.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool showSplash = true;
+  showSplashScreen(){
+    Future.delayed(Duration(seconds: 5),(){
+      setState(() {
+        showSplash = false;
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    showSplashScreen();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'UnSad',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home:  showSplash ? splashScreen() : MainScreen(),
+    );
+  }
+}
